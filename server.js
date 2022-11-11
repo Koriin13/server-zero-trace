59e960503f7155e5263441e3c3cfa34d1fcbf864
koriin13 <106552110+Koriin13@users.noreply.github.com>
2022-11-11 16:01:59 -0500

    Update url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Allow for CORS from http://localhost:3000 GET and POST methods
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: 'https://client-zero-trace.onrender.com/',
       methods: ['GET', 'POST'],
     },
 });
